459188b43eda7d56b812b659ac4783ba38f01778
AndreyKrylov <a.krylov@8bitgroup.com>
2019-01-21 09:43:14 +0300

    Connected to swapi service

--- src/index.js
+++ src/index.js
@@ -4,12 +4,47 @@
 //     console.log(body);
 // });
 
-const getResponse = async (url) => {
-    const response = await fetch(url);
-    const body = response.json();
-    return body;
-};
+class SwapiService {
+    _apiBase = 'https://swapi.co/api';
 
-getResponse('https://swapi.co/api/people/4/').then((body) => {
-    console.log(body);
+    async getResponse (url) {
+        const response = await fetch(`${this._apiBase}${url}`);
+        if (!response.ok) {
+            console.log(`Could not fetch ${response.status}`);
+        }
+
+        return await response.json();
+    };
+
+    async getAllPeople () {
+        const res = await this.getResponse('/people/');
+        return res.results;
+    }
+
+    getPerson (id) {
+        return this.getResponse(`/people/${id}`);
+    }
+
+    async getAllPlanets () {
+        const res = await this.getResponse('/planets/');
+        return res.results;
+    }
+
+    getPlanet (id) {
+        return this.getResponse(`/planets/${id}`);
+    }
+
+    async getAllStarships () {
+        const res = await this.getResponse('/starships/');
+        return res;
+    }
+
+    getStarship (id) {
+        return this.getResponse(`/starships/${id}`);
+    }
+}
+
+const swapi = new SwapiService();
+swapi.getStarship(3).then((body) => {
+    console.log(body.model);
 });
